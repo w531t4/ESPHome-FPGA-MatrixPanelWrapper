96b4f75b926373b52c89f84a933d728c1eeb2f6f
Aaron White <w531t4@gmail.com>
2025-12-11 20:13:47 -0500

    use clang-format

--- components/fpga_matrix_display/number/matrix_display_brightness.cpp
+++ components/fpga_matrix_display/number/matrix_display_brightness.cpp
@@ -1,21 +1,18 @@
 #include "matrix_display_brightness.h"
 
-namespace esphome::matrix_display::matrix_display_brightness
-{
+namespace esphome::matrix_display::matrix_display_brightness {
 
-    void MatrixDisplayBrightness::control(float value)
-    {
-        // Update the display brightness and forward state to all number entities
-        display_->set_brightness((int)value);
-        for (MatrixDisplayBrightness *brightness_value : display_->get_brightness_values())
-        {
-            brightness_value->publish_state(value);
-        }
+void MatrixDisplayBrightness::control(float value) {
+    // Update the display brightness and forward state to all number entities
+    display_->set_brightness((int)value);
+    for (MatrixDisplayBrightness *brightness_value :
+         display_->get_brightness_values()) {
+        brightness_value->publish_state(value);
     }
+}
 
-    void MatrixDisplayBrightness::dump_config()
-    {
-        ESP_LOGCONFIG(TAG, "MatrixDisplayBrightness");
-    }
+void MatrixDisplayBrightness::dump_config() {
+    ESP_LOGCONFIG(TAG, "MatrixDisplayBrightness");
+}
 
 } // namespace esphome::matrix_display::matrix_display_brightness
