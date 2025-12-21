1f61e18466202c90e12e4dff823b9b5d924b7bfe
Aaron White <w531t4@gmail.com>
2025-12-21 17:24:46 -0500

    add license info

--- components/fpga_matrix_display/matrix_display.h
+++ components/fpga_matrix_display/matrix_display.h
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2019 ESPHome
+// SPDX-FileCopyrightText: 2025 Aaron White <w531t4@gmail.com>
+// SPDX-License-Identifier: GPL-3.0-only
 #pragma once
 
 #include <utility>
--- components/fpga_matrix_display/switch/__init__.py
+++ components/fpga_matrix_display/switch/__init__.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: 2019 ESPHome
+# SPDX-FileCopyrightText: 2025 Aaron White <w531t4@gmail.com>
+# SPDX-License-Identifier: MIT
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import switch
